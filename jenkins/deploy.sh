045b25a7fccf510bbf9d177d2bb854fac674e174
Bauer, Michael <Michael.Bauer2@ca.com>
2018-12-18 18:57:31 -0500

    Remove some echo statements

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,7 +5,6 @@ if bright files list ds $TEST_LOADLIB | grep -q $TEST_LOADLIB; then
     echo "Test LOADLIB already exists"
 else
     echo "Test LOADLIB does not exist. Create it."
-    echo 'bright files create bin' $TEST_LOADLIB
     bright files create bin $TEST_LOADLIB
 fi
 
@@ -15,12 +14,8 @@ function submitJCL () {
     ds=$1
 
     jobid=`bright jobs submit data-set $ds --rff jobid --rft string`
-    echo $jobid
-    echo ''
 
     retcode=`bright jobs view job-status-by-jobid $jobid --rff retcode --rft string`
-    echo $retcode
-    echo ''
     
     counter=0
     while (("$counter" < $tries)) && [ "$retcode" == "null" ]; do
@@ -28,21 +23,16 @@ function submitJCL () {
         sleep $wait
         
         retcode=`bright jobs view job-status-by-jobid $jobid --rff retcode --rft string`
-        echo $retcode
-        echo ''
     done
 
     if [ "$retcode" == "null" ]; then
        echo $ds 'timed out'
-       echo ''
        exit 1
     elif [ "$retcode" != "CC 0000" ]; then
        echo $ds 'did not complete with CC 0000'
-       echo ''
        exit 1
     else
        echo 'Success'
-       echo ''
     fi
 }
 
